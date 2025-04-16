8d9f443f163b8b942ea59e81f0d1c3a8d57e6199
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-04-16 09:05:53 +0200

    Add formula for brew from joelpurra/homebrew-joelpurra

---
+++ jqnpm.rb
@@ -0,0 +1,28 @@
+# typed: false
+# frozen_string_literal: true
+
+class Jqnpm < Formula
+    desc "Package manager built for the command-line JSON processor jq"
+    homepage "https://github.com/miguel76/jqnpm"
+    url "https://github.com/miguel76/jqnpm.git", tag: "v0.5.7"
+    license any_of: ["MIT", "BSD-2-Clause", "GPL-3.0-or-later"]
+  
+    head do
+      url "https://github.com/miguel76/jqnpm.git",
+          branch: "jq-package-root"
+      version "v0.5.7-jq-package-root"
+    end
+  
+    depends_on "bash"
+    depends_on "jq"
+    depends_on "shunit2"
+  
+    def install
+      prefix.install "src"
+      bin.install_symlink prefix/"src/jqnpm"
+    end
+  
+    test do
+      assert_match(/install/, shell_output("#{bin}/jqnpm help"))
+    end
+  end
